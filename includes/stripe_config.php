067d1a12fb4b9c0a1687e28ebb72ebac6d6f42c3
Nordize <panupong.wong@gmail.com>
2018-10-30 14:18:10 -0400

    delete API keys

--- includes/stripe_config.php
+++ includes/stripe_config.php
@@ -10,8 +10,8 @@
 require_once('__DIR__/../vendor/autoload.php');
 
 $stripe = array(
-    "secret_key"      => "sk_live_Rw9DZSoSJhDoxgTQfIED0LWS",
-    "publishable_key" => "pk_live_rLy1dp4J9EOKB5SkpLRxbQ9m"
+    "secret_key"      => "sk_live_xxxxxxxxxxxxxxxxxxxxxxxx",
+    "publishable_key" => "pk_live_xxxxxxxxxxxxxxxxxxxxxxxx"
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
